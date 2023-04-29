0e8a7b54b9f10567317ff0ae862289b308ef61be
Rojohn <rojohnibana@gmail.com>
2023-04-29 08:45:58 +0200

    square void added with signature

--- C/geometry/square_void/functions.c
+++ C/geometry/square_void/functions.c
@@ -96,3 +96,27 @@ int	ft_satoi(char *str)
 	}
 	return (number);
 }
+
+/*
+     RRRRR                                                                                                       
+   RRR   RRR                                                                                                     
+  RRR     RRR                                                                                                    
+  RR      RRRRRRRRRRRRRRRR                                                                                       
+  RRR     RRRR          RRRRRR                                                hh                                 
+   RRR    RRR               RRR                                               hh                                 
+     RRRRRRRR               RRRR                                              hh                                 
+          RRR            RRRRR                                                hh                                 
+          RRR            RRR                                                  hh                                 
+          RRR          RRRR                                                   hh               nn                
+         RRR       RRRRR                    ooooooo      jj      ooooooo      hh hhhhhhh       nn nnnnnn         
+         RRRRRRRRRRRRRR                   ooo     ooo    jj    ooo     ooo    hhhh     hhh     nnnn    nnn       
+       RRRR          RRRR               ooo        ooo   jj  ooo        ooo   hh        hh     nn       nn       
+      RRRR             RRR              oo          oo   jj  oo          oo   hh        hh     nn       nn       
+     RRRR               RRR             ooo        ooo   jj  ooo        ooo   hh        hh     nn       nn       
+    RRR                  RRR     RRRRR    ooo     ooo    jj    ooo     ooo    hh        hh     nn       nn   nn  
+  RRRR                    RRRRRRRR          ooooooo      jj      ooooooo      hh         hhhh  nn        nnnn    
+                                                    jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+                                                   jj    jj                                                      
+                                                   jj    jj                                                      
+                                                    jjjjjjj                                                      
+*/
