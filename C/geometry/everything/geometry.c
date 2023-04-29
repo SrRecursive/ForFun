a3cd9c2bfb299f31a692bf7e0ec685f398285029
Rojohn <rojohnibana@gmail.com>
2023-04-29 08:45:26 +0200

    everything added with signature

--- C/geometry/everything/geometry.c
+++ C/geometry/everything/geometry.c
@@ -113,3 +113,27 @@ void	triangleUnfilled(int base)
 		printf("\n");
 	}
 }
+
+/*
+     RRRRR                                                                                                       
+   RRR   RRR                                                                                                     
+  RRR     RRR                                                                                                    
+  RR      RRRRRRRRRRRRRRRR                                                                                       
+  RRR     RRRR          RRRRRR                                                hh                                 
+   RRR    RRR               RRR                                               hh                                 
+     RRRRRRRR               RRRR                                              hh                                 
+          RRR            RRRRR                                                hh                                 
+          RRR            RRR                                                  hh                                 
+          RRR          RRRR                                                   hh               nn                
+         RRR       RRRRR                    ooooooo      jj      ooooooo      hh hhhhhhh       nn nnnnnn         
+         RRRRRRRRRRRRRR                   ooo     ooo    jj    ooo     ooo    hhhh     hhh     nnnn    nnn       
+       RRRR          RRRR               ooo        ooo   jj  ooo        ooo   hh        hh     nn       nn       
+      RRRR             RRR              oo          oo   jj  oo          oo   hh        hh     nn       nn       
+     RRRR               RRR             ooo        ooo   jj  ooo        ooo   hh        hh     nn       nn       
+    RRR                  RRR     RRRRR    ooo     ooo    jj    ooo     ooo    hh        hh     nn       nn   nn  
+  RRRR                    RRRRRRRR          ooooooo      jj      ooooooo      hh         hhhh  nn        nnnn    
+                                                    jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+                                                   jj    jj                                                      
+                                                   jj    jj                                                      
+                                                    jjjjjjj                                                      
+*/
