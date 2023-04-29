b005c0dd442f9546e9e0916ccf5c3943d4849fe8
Rojohn <rojohnibana@gmail.com>
2023-04-29 08:45:46 +0200

    square asterisk added with signature

--- C/geometry/square_asterisk/lib.h
+++ C/geometry/square_asterisk/lib.h
@@ -8,3 +8,27 @@ void	square(int base);
 int		anumber(char *str);
 
 #endif
+
+/*
+     RRRRR                                                                                                       
+   RRR   RRR                                                                                                     
+  RRR     RRR                                                                                                    
+  RR      RRRRRRRRRRRRRRRR                                                                                       
+  RRR     RRRR          RRRRRR                                                hh                                 
+   RRR    RRR               RRR                                               hh                                 
+     RRRRRRRR               RRRR                                              hh                                 
+          RRR            RRRRR                                                hh                                 
+          RRR            RRR                                                  hh                                 
+          RRR          RRRR                                                   hh               nn                
+         RRR       RRRRR                    ooooooo      jj      ooooooo      hh hhhhhhh       nn nnnnnn         
+         RRRRRRRRRRRRRR                   ooo     ooo    jj    ooo     ooo    hhhh     hhh     nnnn    nnn       
+       RRRR          RRRR               ooo        ooo   jj  ooo        ooo   hh        hh     nn       nn       
+      RRRR             RRR              oo          oo   jj  oo          oo   hh        hh     nn       nn       
+     RRRR               RRR             ooo        ooo   jj  ooo        ooo   hh        hh     nn       nn       
+    RRR                  RRR     RRRRR    ooo     ooo    jj    ooo     ooo    hh        hh     nn       nn   nn  
+  RRRR                    RRRRRRRR          ooooooo      jj      ooooooo      hh         hhhh  nn        nnnn    
+                                                    jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+                                                   jj    jj                                                      
+                                                   jj    jj                                                      
+                                                    jjjjjjj                                                      
+*/
