0ff5f76a643914b3291d3fb8dceb37c5abe85d7c
SrRecursive <rojohnibana@gmail.com>
2023-05-31 02:00:12 +0200

    N primo

--- C/MiniProjects/nprimo.c
+++ C/MiniProjects/nprimo.c
@@ -3,13 +3,15 @@
 #include "colores.h"
 
 int	obtener_divisores(int n);
+int	esprimo(int n);
 
 int	main(int argc, char **argv)
 {
-	int		cantidad, n, n_aux;
+	int		cantidad, n, n_aux, contador;
 	char	*endargv;
 
 	n_aux = 1;
+	contador = 1;
 	if (argc > 2)
 	{
 		printf("%sUso con parámetros: %s <n>\n%s", YELLOW, argv[0], RESET);
@@ -29,6 +31,12 @@ int	main(int argc, char **argv)
 		else
 		{
 			n = (int) strtol(argv[1], &endargv, 10);
+
+			if (*endargv != '\0')
+			{
+				printf("%sError: %sNo es un número\n", MAGENTA, RESET);
+				return (0);
+			}
 		}
 		printf("Introduce la cantidad de primos que quieres (0 si quieres todos): ");
 		if (scanf(" %d", &cantidad) == 0)
@@ -37,36 +45,59 @@ int	main(int argc, char **argv)
 			printf("%sError: %sNo es un número\n", MAGENTA, RESET);
 			return (0);
 		}
-		if (cantidad < -1)
+		if (cantidad < 0)
 		{
 			printf("%sError: %sNo es una cantidad válida\n", MAGENTA, RESET);
 			return (0);
 		}
-		while (cantidad--)
+		while ((n_aux <= n))
 		{
-
+			if (contador == cantidad + 1 && cantidad != 0)
+			{
+				break;
+			}
+			if (esprimo(n_aux) == 1)
+			{
+				printf("Primo número %d: %d\n", contador, n_aux);
+				contador++;
+			}
+			if (n )
+			n_aux++;
+		}
+		if (cantidad != 0 && contador < cantidad)
+		{
+			printf("%sError: %sQuerías %d primos, pero el número %d solo tiene %d primos\n",
+					MAGENTA, RESET, cantidad, n, contador - 1);
 		}
 	}
+	return (0);
 }
 
 int	obtener_divisores(int n)
 {
-	int	contador, n_divisores;
+	int	divisor, n_divisores;
 
-	contador = 0;
+	divisor = 1;
 	n_divisores = 0;
-	while ((n / contador) != 0)
+	while ((n / divisor) > 0)
 	{
-		if ((n % contador) == 0)
+		if ((n % divisor) == 0)
 		{
 			n_divisores++;
 		}
-		contador++;
+		divisor++;
 	}
 	return (n_divisores);
 }
 
-int	n_primos(int n)
+int	esprimo(int n)
 {
+	int	n_divisores;
 
+	n_divisores = obtener_divisores(n);
+	if (n_divisores == 2)
+	{
+		return (1);
+	}
+	return (0);
 }
